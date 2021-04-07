b5663de5ee719daef5977d064790a30cd548d57b
wslee950920 <wslee950920@gmail.com>
2021-04-08 02:20:36 +0900

    initial commit

---
+++ ec2.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "gatmauel" {
+  ami = "ami-0078a04747667d409"
+  instance_type = "t2.micro"
+  key_name = "gatmauel"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id, aws_security_group.allow_http.id, aws_security_group.allow_https.id]
+
+  provisioner "remote-exec" {
+      connection {
+        type = "ssh"
+        user = "ubuntu"
+        private_key = file("./gatmauel.pem")
+        host = self.public_ip
+      } 
+
+      inline=[
+          "sudo apt update",
+          "sudo apt install -y software-properties-common",
+          "sudo apt-add-repository --yes --update ppa:ansible/ansible",
+          "sudo apt install -y ansible",
+          "sudo ansible-pull -U https://github.com/wslee950920/gatmauel-ansible docker.yaml -i localhost"
+      ]
+  }
+}
+
---
+++ sg.tf
@@ -0,0 +1,59 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "allow_https" {
+  name        = "allow_https"
+  description = "Allow HTTPS inbound traffic"
+
+  ingress {
+    description = "HTTPS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
