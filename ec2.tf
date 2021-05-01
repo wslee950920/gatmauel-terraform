92ffe2467e00d2a15d98f4053252317d3f2aa452
wslee950920 <wslee950920@gmail.com>
2021-05-02 01:18:50 +0900

    ssh 연결 대신 user_data 이용

--- ec2.tf
+++ ec2.tf
@@ -3,22 +3,12 @@ resource "aws_instance" "gatmauel" {
   instance_type = "t2.micro"
   key_name = "gatmauel"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id, aws_security_group.allow_http.id, aws_security_group.allow_https.id]
-
-  provisioner "remote-exec" {
-      connection {
-        type = "ssh"
-        user = "ubuntu"
-        private_key = file("./gatmauel.pem")
-        host = self.public_ip
-      } 
-
-      inline=[
-          "sudo apt update",
-          "sudo apt install -y software-properties-common",
-          "sudo apt-add-repository --yes --update ppa:ansible/ansible",
-          "sudo apt install -y ansible",
-          "sudo ansible-pull -U https://github.com/wslee950920/gatmauel-ansible gatmauel.yaml -i localhost"
-      ]
-  }
+  user_data = <<EOF
+#!/bin/bash
+apt update
+apt install -y software-properties-common
+apt-add-repository --yes --update ppa:ansible/ansible
+apt install -y ansible
+ansible-pull -U https://github.com/wslee950920/gatmauel-ansible gatmauel.yaml -i localhost
+EOF
 }
-
